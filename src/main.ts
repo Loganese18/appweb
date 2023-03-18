7a962016bbd1587c9766659a3ba96edb10ab190a
Logan <loganshalom@gmail.com>
2023-03-18 05:12:58 -0300

    Cors enabled in main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ const PORT = 3000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   await app.listen(PORT);
   console.log('App running on port', PORT);
 }
